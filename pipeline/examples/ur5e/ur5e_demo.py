08305b73f1fac2f6ec355a39975dceca527a1bce
qrvmil <qrvmil@yandex.ru>
2025-07-30 19:30:20 +0300

    Add: full pipeline

--- pipeline/examples/ur5e/ur5e_demo.py
+++ pipeline/examples/ur5e/ur5e_demo.py
@@ -5,12 +5,13 @@ from pathlib import Path
 current_dir = Path(__file__).parent
 sys.path.append(str(current_dir))
 sys.path.append(str(current_dir.parent))
+sys.path.append(str(current_dir.parent.parent))
 
 from pipeline import Pipeline
 
 def main():
     pipeline = Pipeline(robot_name="ur5e")
-    pipeline.set_targets([[0.3, 0.3, 0.5]])  
+    pipeline.set_targets([[-0.4, 0.4, 0.7], [-0.1, 0.4, 0.7], [0.3, 0.4, 0.7]])  
     pipeline.plan_path()  
     pipeline.run_simulation()
 
