7dcbf60fcff075b6ee047f95b15d2a2428977249
qrvmil <qrvmil@yandex.ru>
2025-08-02 18:28:44 +0300

    Add: fr3 demo

---
+++ pipeline/examples/fr3/fr3_demo.py
@@ -0,0 +1,20 @@
+import sys
+import os
+from pathlib import Path
+
+current_dir = Path(__file__).parent
+sys.path.append(str(current_dir))
+sys.path.append(str(current_dir.parent))
+sys.path.append(str(current_dir.parent.parent))
+
+from pipeline import Pipeline
+
+def main():
+    pipeline = Pipeline(robot_name="fr3")
+    pipeline.set_targets([[-0.5, 0.4, 0.7], [-0.1, 0.4, 0.7], [0.3, 0.4, 0.7]])
+    pipeline.plan_path()  
+    pipeline.run_simulation()
+
+
+if __name__ == "__main__":
+    main()
